f85cb6807afd4e4fdec8b533cd35c760bc893c23
ryotaro-45123 <ryotaroynwa1225@gmail.com>
2023-01-25 23:31:15 +0900

    update: code

--- typescript/react-amplify-map/lambda/hello.js
+++ typescript/react-amplify-map/lambda/hello.js
@@ -3,6 +3,6 @@ exports.handler = async function(event) {
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/plain" },
-        body: `Good Afternoon, CDK! Youve hit ${event.path}\n`
+        body: `Good Night, CDK! Youve hit ${event.path}\n`
     };
 };
